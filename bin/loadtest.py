79939ee6647ed99fa998565c9f54232805720a6c
Raycoms <ray.neiheiser@gmail.com>
2023-09-18 14:20:04 +0200

    adjust

--- bin/loadtest.py
+++ bin/loadtest.py
@@ -72,12 +72,12 @@ def build_pod_template() -> PodTemplate:
                     # is not the bottleneck
                     "resources": {
                         "requests": {
-                            "cpu": "8",
-                            "memory": "10Gi",
+                            "cpu": "20",
+                            "memory": "32Gi",
                         },
                         "limits": {
-                            "cpu": "8",
-                            "memory": "10Gi",
+                            "cpu": "20",
+                            "memory": "32Gi",
                         },
                     },
                 }
@@ -114,8 +114,8 @@ def build_loadtest_command(
         f"--expected-max-txns={20000 * loadtestConfig['duration']}",
         "--txn-expiration-time-secs=" f"{loadtestConfig['txn_expiration_time_secs']}",
         "--max-transactions-per-account=1",
-	    "--workers-per-endpoint=90",
-        "--accounts-per-worker=300",
+	    "--workers-per-endpoint=1500",
+        "--accounts-per-worker=5",
         *(
             ["--transaction-type", "solana"]
             if loadtestConfig["coin_transfer"]
